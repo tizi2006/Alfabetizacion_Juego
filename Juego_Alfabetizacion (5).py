112b8cec75d0e69fc804ca73fb9b37073856ab2b
alcal-8-5 <tmareco@sagradoalcal.edu.ar>
2022-06-28 15:09:55 -0300

    ventana emergente

--- Juego_Alfabetizacion (5).py
+++ Juego_Alfabetizacion (5).py
@@ -5,6 +5,7 @@ import random
 from functools import partial
 import time
 from itertools import cycle
+from tkinter import messagebox
 
 vocales = ["A","E","I","O","U"]
 ventana = tkinter.Tk()
@@ -17,7 +18,7 @@ frame.grid(row=0, column=0)
 frame2.grid(row=1, column=0)
 frame3.grid(row=0, column=0)
 
-contenido = os.listdir('/home/alcal-8-5/Documentos/VERTHI/ninos/fotos/')
+contenido = os.listdir('/home/alcal-8-5/Documentos/VERTHI/Alfabetizacion_Juego/fotos/')
 print(contenido)
 
 photos = cycle(ImageTk.PhotoImage(Image.open('fotos/'+image)) for image in contenido)
@@ -47,9 +48,21 @@ def intermedia():
     sortear(panel)
 
 
+def v2():
+    Ventana4=Toplevel()
+    Ventana4.geometry("200x200")
+    
+
+
+
 for i in range(5):
-    boton1 = tkinter.Button(frame2,text=vocales[i])   
+    boton1 = tkinter.Button(frame2,command=partial(v2),text=vocales[i])   
     boton1.grid(column=i,row=0)
+    
+
+
+
+    
     
     
 boton2 = tkinter.Button(text="siguiente",command=partial(sortear,panel))
